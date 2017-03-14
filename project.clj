364ed716a2bbb2d5bbbb079e6d9eb171fb9fd303
Tim Macdonald <tsmacdonald@gmail.com>
2017-03-13 20:55:51 -0500

    Add actual statistical testing

--- project.clj
+++ project.clj
@@ -4,5 +4,4 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [incanter "1.5.7"]
                  [org.clojure/data.csv "0.1.3"]])
--- src/mackintosh/core.clj
+++ src/mackintosh/core.clj
@@ -4,6 +4,7 @@
             [clojure.string :as str]))
 
 (def separator "::")
+(def alpha 0.05)
 
 (defn convert-key
   [key]
@@ -68,17 +69,56 @@
        (str/join "\n")
        (println)))
 
+(defn combinations ;; borrowed from clojure.math.combinatorics
+  [n k]
+  (cond
+    (< k 0) 0
+    (> k n) 0
+    (zero? k) 1
+    (= k 1) n
+    (> k (quot n 2)) (recur n (- n k))
+    :else (/ (apply *' (range (inc (- n k)) (inc n)))
+             (apply *' (range 1 (inc k))))))
+
+(defn fisher-test
+  [a b c d]
+  (/
+   (*
+    (combinations (+ a b) a)
+    (combinations (+ c d) c))
+   (combinations (+ a b c d) (+ a c))))
+
+(defn test-statistic
+  [[first-freqs second-freqs] [first-key second-key]]
+  (letfn [(get-key [map key]
+            (get
+             (get map (first (keys map)))
+             key
+             0))]
+    (fisher-test
+     (get-key first-freqs first-key)
+     (get-key first-freqs second-key)
+     (get-key second-freqs first-key)
+     (get-key second-freqs second-key))))
+
+(defn significant?
+  [T]
+  (<= T alpha))
+
+(defn test-significance
+  ([header data-by-type column type sources] (test-significance header data-by-type column type sources [true false]))
+  ([header data-by-type column type sources options]
+   (let [index (index-for header column)
+         frequency-list (frequencies-for-column data-by-type index type sources)]
+     (significant? (test-statistic frequency-list options)))))
+
 (defn -main
   ([] (-main "mackintosh.csv"))
   ([filename]
    (let [[header data] (read-csv filename)
          data-by-type (group-by-columns header data ["Type" "Book"])
-         syncopation-index (index-for header "Syncopation?")
-         frequency-list (frequencies-for-column data-by-type syncopation-index "Reel" ["Mackintosh-1" "Mackintosh-2"])]
-     (pretty-print frequency-list))))
-
-
-#_ (
-    {"Reel::Mackintosh-1" {false 8, true 9}}
-    {"Reel::Mackintosh-2" {true 19, false 3}}
-    )
+         fields-of-interest ["Syncopation?" "Trill?" "Missed chance for SbS apart from known exceptions?" "Staccato?" "Slurred Staccato?" "Apoggiatura?" "Triplet" "Dotted triplet" "SSL" "LSS" "Apog. 16Th"]]
+     (map vector
+          fields-of-interest
+          (map #(test-significance header data-by-type % "Reel" ["Mackintosh-1" "Mackintosh-2"])
+               fields-of-interest)))))
